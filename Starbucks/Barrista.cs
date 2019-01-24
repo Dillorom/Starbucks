00c83ae5521802eb9f8b789378a4ad4dfcd8555b
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-24 09:33:10 -0800

    update the Serve function

--- Starbucks/Barrista.cs
+++ Starbucks/Barrista.cs
@@ -16,7 +16,7 @@ namespace Starbucks
             this.inventory = new Inventory();
             this.customer = new Customer();
         }
-      
+
         //public  (bool isAvailable, decimal price) Greeting(string choice)
         //{
         //    if (this.inventory.drinks.Any(drink => drink.Name == choice))
@@ -26,6 +26,8 @@ namespace Starbucks
         //    }
         //    return (false, 0);
         //}
+
+        //Link<FavDrinks> CustomerFavDrinks = this.customer.FavDrinks
 
         public (bool isAvailable, decimal price) Greeting(string choice)
         {
@@ -45,11 +47,18 @@ namespace Starbucks
 
         public void Serve(List<FavDrinks> favDrinks)
         {
+            //foreach(List<Drinks> drinkInInventory in this.inventory.drinks)
+            // {
+            //   foreach(List<FavDrinks> favDrink in favDrinks)
+            // {
+            // if(drinkInInventory.Name == favDrink.Name)
+            //{ 
+            // Drink customerDrink = this.inventory.drinks.Where(drinkInInventory => drinkInInventory.Name == favDrink.Name);
+            //this.inventory.drinks.Remove(customerDrink);
+            //Console.WriteLine($"{customerDrink} is served.");
+            //Console.ReadKey();
+            //  
 
-            Drink customerDrink = this.inventory.drinks.Where(drink => drink.Name == FavDrinks);
-            this.inventory.drinks.Remove(customerDrink);
-            Console.WriteLine($"{customerDrink} is served.");
-            Console.ReadKey();
         }
 
         internal (bool isAvailable, decimal price) Greeting(List<FavDrinks> favDrinks)
