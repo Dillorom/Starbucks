cad485b9731e20e81d0e6704a359d5677342b075
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-24 19:01:51 -0800

    still working on Greeting function

--- Starbucks/Barrista.cs
+++ Starbucks/Barrista.cs
@@ -19,25 +19,50 @@ namespace Starbucks
 
         public (bool isAvailable, decimal price) Greeting(List<FavDrinks> favDrinks)
         {
+            //var favDrinksList = favDrinks.Select(d => d.Name).Intersect(inventory.drinks.Select(d => d.Name));
+            //var result = favDrinks.Where(d => favDrinksList.Contains(d.Name));
+
+            //Dictionary<string, double> tracker = new Dictionary<string, double>();
+            //foreach (Drink inDrink in inventory.drinks)
+            //{
+            //    foreach (var favDrink in favDrinks)
+            //    {
+            //        if (inDrink.Name == favDrink.Name)
+            //        {
+            //            var foundDrink = favDrink;
+            //            return foundDrink;
+            //            //tracker.Add(foundDrink.Name, foundDrink.Price); //return (true, foundDrink.Price);
+            //        }
+            //        return (false, 0);
+
+            //    }
+            //    tracker.Intersect(inDrink.Name);
+            //return (false, 0);
+
+        //}
+
+
+        //use hash to keep track of things, and serve the first available one
+
             foreach (FavDrinks d in favDrinks)
             {
                 if (inventory.drinks.Any(drink => drink.Name == d.Name))
                 {
-                    Drink foundDrink = this.inventory.drinks.Where(drink => drink.Name == d.Name).FirstOrDefault();
+                    var foundDrink = this.inventory.drinks.FirstOrDefault(drink => drink.Name == d.Name);
                     return (true, foundDrink.Price);
                 }
                 return (false, 0);
             }
-            return (true, 10);
+            return (false, 0);
         }         
 
         public  void Serve(List<FavDrinks> favDrinks)
         {
             foreach (var favDrink in favDrinks)
             {
-                if (inventory.drinks.Any(drink => drink.Name == favDrink.Name))
+                if (inventory.drinks.Exists(drink => drink.Name == favDrink.Name))
                 {
-                    Drink customerDrink = inventory.drinks.Where(drink => drink.Name == favDrink.Name).FirstOrDefault(); ;
+                    var customerDrink = inventory.drinks.Where(drink => drink.Name == favDrink.Name).First(); ;
                     inventory.drinks.Remove(customerDrink);
                     Console.WriteLine($"{customerDrink.Name} is served. Items left in the inventory is {inventory.drinks.Count}.");
                  }
