589e34d93db73a58b2cc2cfe60caec91da8c0609
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-25 09:41:16 -0800

    set the foundDrink variable outside the loop to make it accessible in the outer layer

--- Starbucks/Barrista.cs
+++ Starbucks/Barrista.cs
@@ -19,44 +19,29 @@ namespace Starbucks
 
         public (bool isAvailable, decimal price) Greeting(List<FavDrinks> favDrinks)
         {
-            var inDrinksList = inventory.drinks.Select(d => d.Name).Intersect(favDrinks.Select(d => d.Name));
-            var result = inventory.drinks.Where(d => inDrinksList.Contains(d.Name));
-            var foundDrink = result.ToList().First();
+            //without a loop
+            //    var inDrinksList = inventory.drinks.Select(d => d.Name).Intersect(favDrinks.Select(d => d.Name));
+            //    var result = inventory.drinks.Where(d => inDrinksList.Contains(d.Name));
+            //    var foundDrink = result.ToList().First();
+            //    return (true, foundDrink.Price);
+            //}
+
+            //with a loop
+            Drink foundDrink;
+            foreach (FavDrinks d in favDrinks)
+            {
+                if (inventory.drinks.Any(drink => drink.Name == d.Name))
+                {
+                    foundDrink = this.inventory.drinks.FirstOrDefault(drink => drink.Name == d.Name);
+                    break;
+                }
+                return (false, 0);
+            }
             return (true, foundDrink.Price);
-
-
-            //Dictionary<string, double> tracker = new Dictionary<string, double>();
-            //foreach (Drink inDrink in inventory.drinks)
-            //{
-            //    foreach (var favDrink in favDrinks)
-            //    {
-            //        if (inDrink.Name == favDrink.Name)
-            //        {
-            //            var foundDrink = favDrink;
-            //            return foundDrink;
-            //            //tracker.Add(foundDrink.Name, foundDrink.Price); //return (true, foundDrink.Price);
-            //        }
-            //        return (false, 0);
-
-            //    }
-            //    tracker.Intersect(inDrink.Name);
-            //return (false, 0);
-            //}
-
-            //foreach (FavDrinks d in favDrinks)
-            //{
-            //    if (inventory.drinks.Any(drink => drink.Name == d.Name))
-            //    {
-            //        var foundDrink = this.inventory.drinks.FirstOrDefault(drink => drink.Name == d.Name);
-            //        return (true, foundDrink.Price);
-            //    }
-            //    return (false, 0);
-            //}
-            //return (false, 0);
-        }         
-
+        }
+ 
         public  void Serve(List<FavDrinks> favDrinks)
-        {
+            {
             foreach (var favDrink in favDrinks)
             {
                 if (inventory.drinks.Exists(drink => drink.Name == favDrink.Name))
